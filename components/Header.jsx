b9d6076132eba8034073def875e295feba757359
Abdelhadi Omar <abdelhadioumar@gmail.com>
2023-10-24 12:26:20 +0100

    NavLink

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,39 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 
 export default function () {
+    const activeStyle = {
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+        color: '#161616'
+    }
+
+    function activeLink(isActive) {
+        return isActive ? activeStyle : null;
+    }
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-            <Link to="/host">Host</Link>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
+            <NavLink 
+                to="/host"
+                style={({isActive}) => activeLink(isActive)}
+            >
+                Host
+            </NavLink>
+            <NavLink 
+                to="/about"
+                style={({isActive}) => activeLink(isActive)}
+            >
+                About
+            </NavLink>
+            <NavLink 
+                to="/vans"
+                style={({isActive}) => activeLink(isActive)}
+            >
+                Vans
+            </NavLink>
             </nav>
         </header>
     )

133b9dcf5c9e3a49ebcdf345d18f7d9b46cee9aa
Abdelhadi Omar <abdelhadioumar@gmail.com>
2023-10-31 14:50:47 +0100

    useLocation

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,24 +1,35 @@
-import React from "react";
-import { useParams } from 'react-router';
+import React from "react"
+import { Link, useParams, useLocation } from "react-router-dom"
 
-export default function () {
-    const {id} = useParams();
-    const [van, setVan] = React.useState(null);
+export default function VanDetail() {
+    const params = useParams()
+    const location = useLocation()
+    
+    const [van, setVan] = React.useState(null)
 
     React.useEffect(() => {
-        fetch(`/api/vans/${id}`)
+        fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
-            .then(data => setVan(data.vans));
-    }, [id]);
-    
+            .then(data => setVan(data.vans))
+    }, [params.id])
+
+    const path = location.state?.search || '';
     return (
         <div className="van-detail-container">
+            <Link
+                to={`..${path}`}
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
+            
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <i className={`van-type ${van.type} selected`}>
+                        {van.type}
+                    </i>
                     <h2>{van.name}</h2>
-                    <p>${van.price}<span>/day</span></p>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
                     <p>{van.description}</p>
                     <button className="link-button">Rent this van</button>
                 </div>
--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -17,7 +17,10 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
+            <Link
+                to={`${van.id}`}
+                state={{ search: `?${searchParams.toString()}` }}
+            >
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
@@ -32,10 +35,30 @@ export default function Vans() {
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <button onClick={() => setSearchParams({type: "simple"})} className="van-type simple">Simple</button>
-                <button onClick={() => setSearchParams({type: "luxury"})} className="van-type luxury">Luxury</button>
-                <button onClick={() => setSearchParams({type: "rugged"})} className="van-type rugged">Rugged</button>
-                <button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear filter</button>
+                <button
+                    onClick={() => setSearchParams({type: "simple"})}
+                    className={`van-type simple ${typeFilter === 'simple' ? 'selected' : ''}`}
+                >
+                    Simple
+                </button>
+                <button
+                    onClick={() => setSearchParams({type: "luxury"})}
+                    className={`van-type luxury ${typeFilter === 'luxury' ? 'selected' : ''}`}
+                >
+                    Luxury
+                </button>
+                <button
+                    onClick={() => setSearchParams({type: "rugged"})}
+                    className={`van-type rugged ${typeFilter === 'rugged' ? 'selected' : ''}`}
+                >
+                    Rugged
+                </button>
+                {typeFilter ? <button
+                    onClick={() => setSearchParams({})}
+                    className="van-type clear-filters"
+                >
+                    Clear filter
+                </button> : null}
             </div>
             <div className="van-list">
                 {vanElements}

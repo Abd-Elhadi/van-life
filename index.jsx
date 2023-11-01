38c2f8039ad663e21aeb7c7b733f9e525fae0088
Abdelhadi Omar <abdelhadioumar@gmail.com>
2023-11-01 11:01:18 +0100

    NotFound page

--- index.jsx
+++ index.jsx
@@ -15,6 +15,7 @@ import HostVanDetail from './pages/Host/HostVanDetail';
 import HostVanInfo from './pages/Host/HostVanInfo';
 import HostVanPhotos from './pages/Host/HostVanPhotos';
 import HostVanPricing from './pages/Host/HostVansPricing';
+import NotFound from './pages/NotFound';
 
 import "./server"
 
@@ -42,6 +43,7 @@ function App() {
             </Route>
             <Route path="reviews" element={<Reviews />} />
           </Route>
+          <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
